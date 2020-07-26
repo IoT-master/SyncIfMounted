051c31f5e69cf8325fd057a868d3a8c1cd296dc1
Senh Mo Chuang <senhmo@gmail.com>
2020-07-26 12:41:51 -0500

    updated arg name

--- SyncIfMounted.py
+++ SyncIfMounted.py
@@ -8,7 +8,7 @@ parser.add_argument('source', help='path/to/source_folder')
 parser.add_argument('destination', help='path/to/destination_folder')
 parser.add_argument('-z', '--zip_archive_recusive',
                     help='zip transfer, archived, and recursively transfered', action="store_true")
-parser.add_argument('-d', '--delete_after',
+parser.add_argument('-d', '--delete',
                     help='delete files if destination is not in source', action="store_true")
 args = parser.parse_args()
 
@@ -23,14 +23,14 @@ check_for_existance = len(list(filter(lambda x: x in args.source, mount_paths)))
 
 if check_for_existance:
     if args.zip_archive_recusive:
-        if args.delete_after:
+        if args.delete:
             subprocess.run(
                 f'/usr/bin/rsync -zar --delete-during {args.source} {args.destination}', shell=True)
         else:
             subprocess.run(
                 f'/usr/bin/rsync -zar {args.source} {args.destination}', shell=True)
     else:
-        if args.delete_after:
+        if args.delete:
             subprocess.run(
                 f'/usr/bin/rsync --delete-during {args.source} {args.destination}', shell=True)
         else:
